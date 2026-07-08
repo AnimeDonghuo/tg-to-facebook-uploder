53d797b1dbcbcc218966f058da98c4daf2b4ded9
AnimeDonghuo <165592897+AnimeDonghuo@users.noreply.github.com>
2026-07-08 13:05:10 +0530

    Update client.py

--- app/bot/client.py
+++ app/bot/client.py
@@ -2,7 +2,7 @@ from pyrogram import Client
 from app.config import settings
 
 bot = Client(
-    "tg_fb_bot",
+    "tg_fb_uploader",
     api_id=settings.API_ID,
     api_hash=settings.API_HASH,
     bot_token=settings.BOT_TOKEN,
