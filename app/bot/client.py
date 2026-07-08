ec5fb75e4cfd0692f6e94d60b0d639f4addeb9bc
AnimeDonghuo <165592897+AnimeDonghuo@users.noreply.github.com>
2026-07-08 13:38:48 +0530

    Update client.py

--- app/bot/client.py
+++ app/bot/client.py
@@ -1,10 +1,11 @@
 from pyrogram import Client
 from app.config import settings
 
+# Using the full module path "app.bot.handlers" is more reliable for Docker
 bot = Client(
     "tg_fb_uploader",
     api_id=settings.API_ID,
     api_hash=settings.API_HASH,
     bot_token=settings.BOT_TOKEN,
-    plugins=dict(root="app/bot/handlers")
+    plugins=dict(root="app.bot.handlers") 
 )
