ff319f6599d73869679c8ddfe13f067f24510a16
AnimeDonghuo <165592897+AnimeDonghuo@users.noreply.github.com>
2026-07-08 13:04:15 +0530

    Update client.py

--- app/facebook/client.py
+++ app/facebook/client.py
@@ -1,15 +1,23 @@
 import aiohttp
 from app.config import settings
+from app.facebook.errors import classify_fb_error
 
-class MetaAPI:
+class MetaGraphClient:
     def __init__(self):
         self.base_url = f"https://graph.facebook.com/{settings.GRAPH_API_VERSION}"
 
-    async def get_page(self, token: str, page_id: str = "me"):
+    async def request(self, method, endpoint, token, params=None, data=None):
+        if params is None: params = {}
+        params["access_token"] = token
         async with aiohttp.ClientSession() as session:
-            url = f"{self.base_url}/{page_id}"
-            params = {"access_token": token, "fields": "id,name,access_token"}
-            async with session.get(url, params=params) as r:
-                return await r.json()
+            async with session.request(method, f"{self.base_url}/{endpoint}", params=params, json=data) as r:
+                res = await r.json()
+                if r.status != 200:
+                    raise classify_fb_error(res)
+                return res
 
-meta_api = MetaAPI()
+    async def get_page_details(self, token: str):
+        # Validates token and gets ID/Name
+        return await self.request("GET", "me", token, params={"fields": "id,name"})
+
+fb_api = MetaGraphClient()
